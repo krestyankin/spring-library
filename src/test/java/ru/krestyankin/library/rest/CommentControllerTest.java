7f5bae7d40b80f3ec72c5bcb141ffe0a4a936d82
Dmitry Krestyankin <d.krestyankin@tinkoff.ru>
2020-04-12 21:33:27 +0300

    OTUS homework 10 jQuery

--- src/test/java/ru/krestyankin/library/rest/CommentControllerTest.java
+++ src/test/java/ru/krestyankin/library/rest/CommentControllerTest.java
@@ -5,7 +5,9 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.krestyankin.library.models.Book;
 import ru.krestyankin.library.models.Comment;
 import ru.krestyankin.library.repositories.AuthorRepository;
@@ -15,8 +17,8 @@ import ru.krestyankin.library.repositories.GenreRepository;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CommentController.class)
@@ -33,17 +35,25 @@ class CommentControllerTest {
     private GenreRepository genreRepository;
 
     @Test
-    void savePage() throws Exception {
+    void getAll() throws Exception {
+        mvc.perform(get("/comment/get/{bookId}", "book1")).andExpect(status().isOk());
+        Mockito.verify(commentRepository, times(1)).findAllByBook("book1");
+    }
+
+    @Test
+    void add() throws Exception {
         Book book = new  Book("book1", "Book title", null, null);
         given(bookRepository.findById(book.getId())).willReturn(java.util.Optional.of(book));
         Comment comment = new Comment("Text", book);
-        mvc.perform(post("/comment/save").param("bookId", comment.getBook().getId()).param("text", comment.getText())).andExpect(status().isOk());
+        mvc.perform(post("/comment/add").param("bookId", comment.getBook().getId()).param("text", comment.getText())).andExpect(status().isOk());
         Mockito.verify(commentRepository, times(1)).save(comment);
     }
 
     @Test
-    void deletePage() throws Exception {
-        mvc.perform(get("/comment/delete").param("id", "cooment1")).andExpect(status().isOk());
-        Mockito.verify(commentRepository, times(1)).deleteById("cooment1");
+    void delete() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.delete("/comment/delete/{commentId}", "comment1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        Mockito.verify(commentRepository, times(1)).deleteById("comment1");
     }
 }
