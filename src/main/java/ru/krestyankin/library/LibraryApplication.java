94a9f4740f511af570b6d06e276a119093ca6a5e
Dmitry Krestyankin <d.krestyankin@tinkoff.ru>
2020-04-22 23:48:44 +0300

    OTUS homework 13 small correction

--- src/main/java/ru/krestyankin/library/LibraryApplication.java
+++ src/main/java/ru/krestyankin/library/LibraryApplication.java
@@ -1,19 +1,15 @@
 package ru.krestyankin.library;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.security.acls.jdbc.JdbcAclService;
 
 @SpringBootApplication
 @EnableMongoRepositories
 public class LibraryApplication {
 		public static void main(String[] args) throws Exception {
 			ApplicationContext context = SpringApplication.run(LibraryApplication.class, args);
-			Log log = LogFactory.getLog(JdbcAclService.class);
 	}
 
 }
